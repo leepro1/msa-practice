5f5d18c4816d04cfee8ee29206700fdac2a6676b
HuiJu <leemoon63@gmail.com>
2024-09-08 12:28:57 +0900

    Feat: gateway header filter 추가

--- first-service/src/main/java/com/msa/first_service/FirstServiceController.java
+++ first-service/src/main/java/com/msa/first_service/FirstServiceController.java
@@ -1,15 +1,20 @@
 package com.msa.first_service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/first-service")
+@Slf4j
 public class FirstServiceController {
 
     @GetMapping("/welcome")
-    public String welcome() {
+    public String welcome(@RequestHeader("first-request") String header) {
+        log.info(header);
         return "Welcome first service";
     }
+
 }
