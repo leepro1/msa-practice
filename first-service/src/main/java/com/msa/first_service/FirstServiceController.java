0e6f1a69f73c1ec0703235c107b48ccd374d695b
HuiJu <leemoon63@gmail.com>
2024-09-08 14:16:46 +0900

    Feat: Gateway - Eureka 연동 및 로드밸런싱 설정

--- first-service/src/main/java/com/msa/first_service/FirstServiceController.java
+++ first-service/src/main/java/com/msa/first_service/FirstServiceController.java
@@ -1,6 +1,9 @@
 package com.msa.first_service;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/first-service")
+@RequiredArgsConstructor
 @Slf4j
 public class FirstServiceController {
+
+    final Environment env;
 
     @GetMapping("/welcome")
     public String welcome(@RequestHeader("first-request") String header) {
@@ -18,8 +24,11 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Check first service";
+    public String check(HttpServletRequest request) {
+        log.info("Server port={}", request.getServerPort());
+
+        return String.format("Check first service on PORT %s",
+            env.getProperty("local.server.port"));
     }
 
 }
