7046afde3aaed39d0b99d269d39c3e7258432eb7
이희주 <leemoon63@gmail.com>
2024-10-05 17:16:16 +0900

    Feat: implement config service

--- user-service/src/main/java/com/msa/user_service/controller/UserController.java
+++ user-service/src/main/java/com/msa/user_service/controller/UserController.java
@@ -6,7 +6,6 @@ import com.msa.user_service.dto.response.UserResponse;
 import com.msa.user_service.service.UserService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,11 +25,18 @@ public class UserController {
 
     @GetMapping("/health-check")
     public String status() {
-        return String.format("It's Working in User Service On PORT %s",
-            env.getProperty("local.server.port"));
+//        return String.format("It's Working in User Service On PORT %s",
+//            env.getProperty("local.server.port"));
+
+        return String.format("env tester"
+            + ". port(local.server.port)= " + env.getProperty("local.server.port")
+            + ". port(server.port)= " + env.getProperty("server.port")
+            + ". token secret= " + env.getProperty("spring.jwt.secret")
+        );
+
     }
 
-    @GetMapping("/welcome")
+    @GetMapping("users/welcome")
     public String welcome(@RequestHeader("X-User-Id") String userId,
         @RequestHeader("X-Username") String username) {
 //        return env.getProperty("greeting.welcome");
